ea53a69cff8be6c844fe58ce38ea4d73fc21d9d6
scirin <irine.kokilashvili@gmail.com>
2024-04-15 21:35:45 +0400

    [feature] Init S3 event trigger

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,53 @@
 use aws_lambda_events::event::s3::S3Event;
-use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+use aws_sdk_s3::Client;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<S3Event>) -> Result<(), Error> {
-    // Extract some useful information from the request
-
-    Ok(())
-}
-
+/// Main function
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .without_time()
+        .init();
 
-    run(service_fn(function_handler)).await
+    // Initialize the AWS SDK for Rust
+    let config = aws_config::load_from_env().await;
+    let s3_client = Client::new(&config);
+
+    let res = run(service_fn(|request: LambdaEvent<S3Event>| {
+        function_handler(&s3_client, request)
+    })).await;
+
+    res
+}
+
+async fn function_handler(
+    s3_client: &Client,
+    evt: LambdaEvent<S3Event>
+) -> Result<(), Error> {
+    tracing::info!(records = ?evt.payload.records.len(), "Received request from SQS");
+
+    if evt.payload.records.len() == 0 {
+        tracing::info!("Empty S3 event received");
+    }
+
+    let bucket = evt.payload.records[0].s3.bucket.name.as_ref().expect("Bucket name to exist");
+    let key = evt.payload.records[0].s3.object.key.as_ref().expect("Object key to exist");
+
+    tracing::info!("Request is for {} and object {}", bucket, key);
+
+    let s3_get_object_result = s3_client
+        .get_object()
+        .bucket(bucket)
+        .key(key)
+        .send()
+        .await;
+
+    match s3_get_object_result {
+        Ok(_) => tracing::info!("S3 Get Object success, the s3GetObjectResult contains a 'body' property of type ByteStream"),
+        Err(_) => tracing::info!("Failure with S3 Get Object request")
+    }
+
+    Ok(())
 }
